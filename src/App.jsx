368a86a23f24cdef0c127b20642b39181e0632d4
Samitha Bandara Wijekoon <shanukab543@gmail.com>
2026-01-01 22:57:33 +0530

    updated property page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import PropertySearch from './Components/PropertySearch/PropertySearch'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+      <PropertySearch />
+    </>
   )
 }
 
